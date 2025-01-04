2419ae45057e677c51d93c65e2c73807c0c493ee
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-04 15:13:45 +0300

    feature: card component created

---
+++ frontend/src/components/ui/card.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+interface CardProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function Card({ className = "", children }: CardProps) {
+  return (
+    <div
+      className={`rounded-lg border  shadow-sm border-gray-800 bg-gray-800 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+interface CardHeaderProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function CardHeader({ className = "", children }: CardHeaderProps) {
+  return <div className={`p-6 ${className}`}>{children}</div>;
+}
+
+interface CardTitleProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function CardTitle({ className = "", children }: CardTitleProps) {
+  return (
+    <h3
+      className={`text-lg font-semibold leading-none tracking-tight ${className}`}
+    >
+      {children}
+    </h3>
+  );
+}
+
+interface CardContentProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export function CardContent({ className = "", children }: CardContentProps) {
+  return <div className={`p-6 pt-0 ${className}`}>{children}</div>;
+}
--- frontend/src/components/ui/logo.tsx
+++ frontend/src/components/ui/logo.tsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 interface LogoProps {
   className?: string;
   showText?: boolean;
+  href?: string;
 }
 
-export function Logo({ className = "", showText = true }: LogoProps) {
+export function Logo({ className = "", showText = true, href }: LogoProps) {
   return (
-    <Link href="/" className={`flex items-center space-x-2 ${className}`}>
+    <Link
+      href={href ? href : "/"}
+      className={`flex items-center space-x-2 ${className}`}
+    >
       <Shield className="h-8 w-8 text-purple-500" />
       {showText && (
         <span className="text-2xl font-light text-purple-500 ">Vaultify</span>
