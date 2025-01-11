a69590d668566cdff0a0f88aff1cd7259924f6d4
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-11 23:23:06 +0300

    refactor: favico road corrected.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,17 +12,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/favicon.ico",
-        sizes: "any",
-      },
-      {
-        url: "/icon.svg",
+        url: "/favicon.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: {
-      url: "/apple-touch-icon.png",
-    },
   },
 };
 
