deba612963e4385806be7686ace8a6c3ee027953
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-18 22:11:17 +0300

    feat: add card encryption and decryption functions. closes #17

--- backend/src/utils/encryption.ts
+++ backend/src/utils/encryption.ts
@@ -10,6 +10,15 @@ export const decryptPassword = (encryptedPassword: string): string => {
   return bytes.toString(CryptoJS.enc.Utf8);
 };
 
+export const encryptData = (data: string): string => {
+  return CryptoJS.AES.encrypt(data, config.aesSecretKey).toString();
+};
+
+export const decryptData = (encryptedData: string): string => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, config.aesSecretKey);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+
 export const hashPassword = async (password: string): Promise<string> => {
   const bcrypt = await import("bcryptjs");
   const salt = await bcrypt.genSalt(10);
