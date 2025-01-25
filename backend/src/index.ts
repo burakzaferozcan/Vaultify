298a93db03f5b0105bc05b818be6f2505085aff8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-01-25 19:16:44 +0300

    refactor: improve server initialization code in index.ts. closes #32

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import authRoutes from "./routes/auth";
 import passwordRoutes from "./routes/passwords";
 import activityRoutes from "./routes/activity";
 import cardRoutes from './routes/cardRoutes';
+import testRoutes from './routes/testRoutes';
+import { startNotificationCron } from './cron/notificationCron';
 
 const app = express();
 
@@ -18,11 +20,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/passwords", passwordRoutes);
 app.use("/api/activity", activityRoutes);
 app.use('/api/cards', cardRoutes);
+app.use("/api/test", testRoutes);
 
 // DB Connection
 connectDB().then(() => {
   // Server
   app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
+    startNotificationCron();
   });
 });
